89c9da29574156f27af4026647dd2aad13b504f4
dec1218 <decflorhenuy.202301455@gmail.com>
2024-10-07 18:46:28 +0800

    update

---
+++ script.js
@@ -0,0 +1,18 @@
+// Initialize EmailJS with your user ID
+(function(){
+    emailjs.init("ImM20PyOwGxTbR4iB");
+})();
+
+
+// Function to send email using EmailJS
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Send the email using the form data
+    console.log(this)
+    emailjs.sendForm('service_b4ik7km', 'template_el51bp6', this)
+        .then(function() {
+            alert('Message Sent Successfully!');
+        }, function(error) {
+            alert('Failed to send the message: ' + JSON.stringify(error));
+        });
+});
